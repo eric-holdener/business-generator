744127bdeb9a6f7e07d3b7496827331db73337c6
EHoldener <eholdener12@gmail.com>
2022-09-06 17:06:15 -0700

    started parallaxing

--- src/components/defaults/Layout.js
+++ src/components/defaults/Layout.js
@@ -3,11 +3,8 @@ import Header from "../header/Header";
 
 export default function Layout() {
   return (
-    <div className="relative">
-      <Header />
-      <div className="pt-14 px-3">
-        <Outlet />
-      </div>
+    <div className="relative w-screen h-screen">
+      <Outlet />
     </div>
   )
 }
--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -1,5 +1,26 @@
+import { Parallax, ParallaxLayer } from "@react-spring/parallax"
+import { useRef } from "react";
+
 export default function Home() {
+  const ref = useRef();
   return (
-    <h1>Home</h1>
+    <Parallax pages={3} ref={ref}>
+      <ParallaxLayer offset={0} speed={2.5}>
+        <div className="bg-gradient-to-br from-cyan-500 to-blue-500 bg-screen h-screen justify-center items-center flex">
+          <h1 className="text-7xl italic font-bold text-white">Definitely a Real Business</h1>
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={1} speed={1}>
+        <div className="bg-gradient-to-br from-blue-500 to-indigo-500 bg-screen h-screen justify-center items-center flex">
+          <h1 className="text-7xl italic font-bold text-white">Look at all our real employees</h1>
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={2} speed={2}>
+        <div className="bg-gradient-to-br from-indigo-500 to-violet-500 bg-screen h-screen justify-center items-center flex">
+          <h1 className="text-7xl italic font-bold text-white">And our very real motto</h1>
+        </div>
+      </ParallaxLayer>
+    </Parallax>
+
   )
 }
